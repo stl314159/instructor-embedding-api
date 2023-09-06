b8c33bb5bb16ef807f7e7ffbd24aef8f2e0da282
TollanBerhanu <tollanberhanu@gmail.com>
2023-09-06 11:31:16 +0300

    fix:: remove !

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 # Install Instructor Embedding and Chroma
-!pip install langchain sentence_transformers InstructorEmbedding
+pip install langchain sentence_transformers InstructorEmbedding
 # Install flask cors to enable cors for all domains.
-!pip install -U flask flask-cors
+pip install -U flask flask-cors
